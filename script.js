ca6243e3d757feb17dcc68e476ee8c61fc83d706
mzulya <sul.zulya@gmail.com>
2020-06-18 13:23:14 +0700

    change JS file

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const date = new Date();
-const username = window.location.search.split('=')[1];
-const url = `https://api.github.com/users/${username}`;
-let username = '?username=mszulya';
+let getUsername = () => {
+	const username = window.location.search.split('username=')[1];
+	if (username == undefined){
+        username = 'mszulya';
+    }
+    return username
+}
+
+const userUrl = `https://api.github.com/users/${username}`;
+console.log(userUrl);
 
 const getDate = new Promise((resolve, reject) => {
+	const date = new Date();
 	setTimeout (() => date ? resolve(date) : reject('no date'), 3000 );
 });
 
-const getUrl = new Promise((resolve, reject) => {
-	setTimeout (() => url ? resolve(url) : reject('no url'), 5000 );
+const getUserUrl = new Promise((resolve, reject) => {
+	setTimeout (() => userUrl ? resolve(userUrl) : reject('no url'), 5000 );
 });
 
 const preloader = document.querySelector('.circ');
@@ -16,15 +23,10 @@ setTimeout(() => {
   preloader.style.display = 'none';
 }, 4999);
 
-Promise.all([getDate, getUrl])
- .then(([date, url]) => fetch(url))
+Promise.all([getDate, getUserUrl])
+  .then(([date, url]) => fetch(userUrl))
 	.then(res => res.json())
 	.then(json => {
-		console.log(json.avatar_url);
-		console.log(json.name);
-		console.log(json.bio);
-		console.log(json.url);
-
 		const avatar = json.avatar_url;
 		const name = json.name;
 		const bio = json.bio;
@@ -32,6 +34,11 @@ Promise.all([getDate, getUrl])
 
 		const body = document.body;
 
+		const nowDate = document.createElement('p');
+		nowDate.innerHTML = date;
+		body.appendChild(nowDate);
+
+		console.log(nowDate);
 		const newPhoto = new Image();
 		newPhoto.src = avatar;
 		body.appendChild(newPhoto);
